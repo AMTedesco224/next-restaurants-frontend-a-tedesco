3dc4a007370baf4a3ea00ceb71cf7764b70da6c4
Anthony Tedesco <amtedesco224@yahoo.com>
2023-03-30 18:02:28 -0400

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is an app to find latest live music events</p>
+        <p>version 1.0.0</p>
+    </div>
+  );
+}
